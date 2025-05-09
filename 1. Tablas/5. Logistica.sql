11bf067c0138e16a23b77592a0cddd748531b0c2
EverUniAndes <eo.martinez2@uniandes.edu.co>
2025-05-08 22:51:20 -0500

    objetos para la ubicación de productos en bodega

--- 1. Tablas/5. Logistica.sql
+++ 1. Tablas/5. Logistica.sql
@@ -5,3 +5,24 @@ CREATE TABLE tbl_inventario (
   fecharegistro timestamp(6) NOT NULL,
   fechaactualizacion timestamp(6)
 );
+
+
+CREATE TABLE tbl_bodegas (
+  id SERIAL PRIMARY KEY,
+  idciudad UUID REFERENCES tbl_ciudades(id),
+  nombre VARCHAR(20) NOT NULL,
+  fecharegistro timestamp(6) NOT NULL,
+  fechaactualizacion timestamp(6)
+);
+
+
+CREATE TABLE tbl_productosbodega (
+  id SERIAL PRIMARY KEY,
+  idbodega INT REFERENCES tbl_bodegas(id),
+  idproducto INT REFERENCES tbl_productos(id),
+  pasillo INT NOT NULL,
+  estante INT NOT NULL,
+  nivel INT NOT NULL,
+  fecharegistro timestamp(6) NOT NULL,
+  fechaactualizacion timestamp(6)
+);
